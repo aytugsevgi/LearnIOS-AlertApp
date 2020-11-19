0100df0bf1b376b1edb6fa684a86efa36b440b83
aytug.sevgi <aytug3667@gmail.com>
2020-11-19 15:26:33 +0300

    first commit

--- AlertApp/ViewController.swift
+++ AlertApp/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var password2Text: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signUpClicked(_ sender: Any) {
+        if usernameText.text!.isEmpty{
+            showAlert(title: "Error", message: "Username can not be empty")
+        }
+        else if passwordText.text!.isEmpty{
+            showAlert(title: "Error", message: "Password can not be empty")
+        }
+        else if passwordText.text! != password2Text.text! {
+            showAlert(title: "Error", message: "Passwords did not match")
+        }
+        else{
+            showAlert(title: "Success", message: "Sign up succesfully completed")
+        }
+        
+    }
+    
+    func showAlert(title : String, message : String){
+        let alertMessage = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default) { (UIAlertAction) in
+            self.usernameText.text = ""
+            self.passwordText.text = ""
+            self.password2Text.text = ""
+        }
+        alertMessage.addAction(okButton)
+        self.present(alertMessage, animated: true, completion: nil)
+    }
+    
 }
 
